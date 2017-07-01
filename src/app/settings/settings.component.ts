b8ee2691fe9a703d78111050a00aae151a7dfec5
Andrei Radzetski <andrei@MacBook-Pro-Andrei.local>
2017-07-01 10:35:43 +0300

    Edit sync section

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -4,4 +4,27 @@ import { Component } from '@angular/core';
   selector: 'lgsc-settings',
   templateUrl: 'settings.component.html'
 })
-export class SettingsComponent { }
+export class SettingsComponent {
+
+  autoSync: boolean = false;
+
+  syncDate: string;
+
+  every: string;
+
+  constructor() {
+    this.updareSyncDate();
+  }
+
+  sync() {
+    this.updareSyncDate();
+    console.log(this.autoSync);
+    console.log(this.syncDate);
+    console.log(this.every);
+  }
+
+  private updareSyncDate() {
+    this.syncDate = new Date().toISOString();
+  }
+
+}
